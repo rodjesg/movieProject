7a87f6af6ecf8e9970f84084024492f048479e4b
Rodney Geerlings <rodney@rgeerlings.com>
2016-10-04 00:06:26 +0200

    First

---
+++ src/main/java/edu/avans/movieproject/presentation/MovieOverviewUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.avans.movieproject.presentation;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import edu.avans.movieproject.domain.Movie;
+
+/**
+ *
+ * @author rodneygeerlings
+ */
+public class MovieOverviewUI {
+    test
+}
