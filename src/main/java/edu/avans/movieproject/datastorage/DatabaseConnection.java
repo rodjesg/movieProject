fda5cba60a41f28bfdfc907fcc1311ecf9681906
Rodney Geerlings <rodney@rgeerlings.com>
2016-10-06 15:57:18 +0200

    Update

--- src/main/java/edu/avans/movieproject/datastorage/DatabaseConnection.java
+++ src/main/java/edu/avans/movieproject/datastorage/DatabaseConnection.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.avans.movieproject.datastore;
+package edu.avans.movieproject.datastorage;
 
 /**
  *
