500fd8ce87c479cb1adc0d7efbfbd71deef1066c
Rodney Geerlings <rodjesg@users.noreply.github.com>
2018-10-29 12:49:51 +0100

    Gui

---
+++ src/main/java/edu/avans/movieproject/main/OverviewGUI.java
@@ -0,0 +1,17 @@
+package edu.avans.movieproject.main;
+
+import edu.avans.movieproject.businesslogic.MovieManager;
+
+import javax.swing.*;
+import java.awt.Panel;
+
+public class OverviewGUI extends JFrame {
+
+    private JPanel panel = new JPanel();
+    private JLabel img;
+    public OverviewGUI(MovieManager movieManager){
+
+    }
+
+
+}
